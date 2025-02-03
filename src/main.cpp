7b8f24f17ff38d8d2c591c8770baf5ad9c769d49
Ryan Williams <ryan.rpwilliams@gmail.com>
2025-02-03 14:00:09 -0500

    .

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,8 @@ int main() {
   std::cout << "Starting OptiTrade Market Data Handler..." << std::endl;
 
   MarketDataHandler marketData;
-  marketData.connect(
-      "ws://test.deribit.com/ws/api/v1/"); // Deribit WebSocket API
-                                            // (wss://www.deribit.com/ws/api/v1/)
+  marketData.connect("ws://fstream.binance.com");
+  // (wss://www.deribit.com/ws/api/v1/)
 
   std::thread data_thread(&MarketDataHandler::run, &marketData);
   data_thread.join(); // Run market data handler
